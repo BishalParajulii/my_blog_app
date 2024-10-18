e05579f9c48a36e0a810a4a392024cbb3fbaa2da
BishalParajulii <parajulibishal00@gmail.com>
2024-10-18 11:58:40 +0545

    working on profile

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -126,3 +126,5 @@ STATIC_ROOT = (BASE_DIR / 'asset' )
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_COOKIE_SECURE = False
